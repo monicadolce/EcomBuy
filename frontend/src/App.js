3f86349019281fdbf97a0f901cf4d35754d9a1b4
Monica <monicadolcemascolo@gmail.com>
2023-03-14 12:24:15 -0400

    worked on homescreen.js, productscreen.js, app.js, store.js, messagebox.js, index.js, utils.js, loadingbox.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 // import data from './data';
 // import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+// import Link from 'react-bootstrap/Link';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Store } from './Store';
+import { useContext } from 'react';
 
 function App() {
+  
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
     <div className="d-flex flex-column site-container">
@@ -17,6 +25,16 @@ function App() {
       <LinkContainer to="/">
         <Navbar.Brand>ecombuy</Navbar.Brand>
         </LinkContainer>
+        <Nav className="me-auto">
+          <Link to="/cart" className="nav-link">
+            cart
+            {cart.cartItems.length > 0 && (
+              <Badge pill bg="danger">
+                {cart.cartItems.length}
+              </Badge>
+            )}
+          </Link>
+          </Nav>
       </Container>
         </Navbar>
       </header>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useReducer } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,6 +9,10 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Rating from "../components/Rating";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
+import { Store } from "../Store";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,17 +43,22 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
-  return loading ? (
-    <div>Loading...</div>
-  ) : error ? (
-    <div>{error}</div>
-  ) : (
+  const {state, dispatch: ctxDispatch} = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity: 1}})
+  }
+
+  return  loading ? (
+    <LoadingBox />
+   ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+   ) : (
     <div>
         <Row>
             <Col md={6}>
@@ -105,7 +114,7 @@ function ProductScreen() {
                             {product.countInStock > 0 && (
                                 <ListGroup.Item>
                                     <div className="d-grid">
-                                        <Button variant="primary">
+                                        <Button onClick={addToCartHandler} variant="primary">
                                             Add to Cart
                                         </Button>
                                     </div>
